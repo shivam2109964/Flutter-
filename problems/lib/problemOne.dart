cda5e083fccce5ea93c28d839d619e231fae8af4
Shivam Gupta <sg2109964@gmail.com>
2025-07-01 17:31:26 +0530

    Problem One Done

--- problems/lib/problemOne.dart
+++ problems/lib/problemOne.dart
@@ -1,8 +1,24 @@
 // Given an array arr. The task is to find the largest element in the given array.
+import 'dart:math';
+
 void main() {
   List<int> array = [45, 78, 59, 12, 14, 36, 52, 46, 75, 95];
   int result = getLargest(array, array.length);
   print("Largest value is: $result");
+  int resultByRec = recLargest(array);
+  print("Largest Value by Recursion: $resultByRec");
+}
+
+int recLargest(List<int> array) {
+  return findMax(array, 0);
+}
+
+int findMax(List<int> array, int index) {
+  if (index == array.length - 1) {
+    return array[index];
+  }
+  int recMax = findMax(array, index + 1);
+  return max(recMax, array[index]);
 }
 
 int getLargest(List<int> array, int length) {
